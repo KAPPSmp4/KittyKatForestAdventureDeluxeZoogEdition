ae74d6e36e46d25928b88a8fdea06a888843bb63
Kapps <mpaci04@gmail.com>
2026-01-22 00:51:23 +0100

    added walk animations and cleaned up scipts

--- Assets/Zoooog/Code/Scripts/PlayerController.cs
+++ Assets/Zoooog/Code/Scripts/PlayerController.cs
@@ -1,21 +1,26 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
+using System.Collections;
 
-[DefaultExecutionOrder(-1)]
+
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
 public class PlayerController : MonoBehaviour
 {
     [Header("Components")]
-    [SerializeField] private CharacterController _characterController;
-    [SerializeField] private Camera _playerCamera;
+    [SerializeField] public CharacterController controller;
+    [SerializeField] public Transform cam;
+    [SerializeField] public Animator animator;
 
     [Header("Movement Settings")]
-    public float walkSpeed = 4f;
-    float turnSmoothTime;
+    [SerializeField] public float speed = 6;
+    private float gravity = -9.81f;
+    private Vector3 velocity;
 
-    [Header("Camera Settings")]
-    public float lookSenseH = 0.1f;
-    public float lookSenseV = 0.1f;
-    public float lookLimitV = 89f;
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
 
     private PlayerLocomotionInput _playerLocomotionInput;
 
@@ -24,48 +29,35 @@ public class PlayerController : MonoBehaviour
         _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        //walk
         Vector3 direction = new Vector3(_playerLocomotionInput.MovementInput.x, 0f, _playerLocomotionInput.MovementInput.y).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _playerCamera.transform.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        /*
-        Vector3 cameraForwardXZ = new Vector3(_playerCamera.transform.forward.x, 0f, _playerCamera.transform.forward.z).normalized;
-        Vector3 cameraRightXZ = new Vector3(_playerCamera.transform.right.x, 0f, _playerCamera.transform.right.z).normalized;
-        Vector3 movementDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y;
 
-        Vector3 movementDelta = movementDirection * runAcceleration * Time.deltaTime;
-        Vector3 newVelocity = _characterController.velocity + movementDelta;
-
-        // Add drag to player 
-        Vector3 currentDrag = newVelocity.normalized * drag * Time.deltaTime;
-        newVelocity = (newVelocity.magnitude > drag * Time.deltaTime) ? newVelocity - currentDrag : Vector3.zero;
-        newVelocity = Vector3.ClampMagnitude(newVelocity, runSpeed);
-        */
-
-        // Move character
-       // _characterController.Move( * Time.deltaTime);
-
-        //movementDirection.x * 90 (left and right)
-        /*
-        float z_value = movementDirection.z;
-        if (z_value > 0 && movementDirection.x <= 0) z_value = 0;
-        if (z_value < 0 && movementDirection.x >= 0) z_value = 0;
-        transform.rotation = Quaternion.Euler(0f, movementDirection.x * 90 + Mathf.Abs(z_value) * 180, 0f);
-        */
-
-    }
-
-    private void LateUpdate()
-    {
-        /*
-        _cameraRotation.x += lookSenseH * _playerLocomotionInput.LookInput.x;
-        _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - lookSenseV * _playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
-        _playerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0f);
-        */
+        //animations
+        if (direction.magnitude == 0f)
+        {
+            animator.SetFloat("speed", 0.0f);
+        }
+        else
+        {
+            animator.SetFloat("speed", 0.5f);
+        }
         
+
     }
 }
--- Assets/Zoooog/Code/Scripts/SpriteDirectionController.cs
+++ Assets/Zoooog/Code/Scripts/SpriteDirectionController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class SpriteDirectionController : MonoBehaviour
 {
-    [SerializeField] float backAngle = 65f;
-    [SerializeField] float sideAngle = 155f;
+    [SerializeField] float backAngle = 36f;
+    [SerializeField] float backLAngle = 72f;
+    [SerializeField] float sideAngle = 108f;
+    [SerializeField] float frontLAngle = 144f;
     [SerializeField] Transform mainTransform;
     [SerializeField] Animator animator;
     [SerializeField] SpriteRenderer spriteRenderer;
@@ -14,15 +16,30 @@ public class SpriteDirectionController : MonoBehaviour
 
         float signedAngle = Vector3.SignedAngle(mainTransform.forward, camForwardVector, Vector3.up);
 
-        Vector2 animationDirection = new Vector2(0f, -1f);
+        Vector2 animationDirection = new Vector2(0.1f, -1f);
 
         float angle = Mathf.Abs(signedAngle);
-
-
+        
         if (angle < backAngle)
         {
             // Back animation
-            animationDirection = new Vector2(0f, -1f);
+            animationDirection = new Vector2(0.1f, -1f);
+        }
+        else if (angle < backLAngle)
+        {
+            // Side animation, in this case the right animation
+            animationDirection = new Vector2(1f, -1f);
+
+            // This changes the side animation based on what side 
+            // the camera is viewing the slime from
+            if (signedAngle < 0f)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
         }
         else if (angle < sideAngle)
         {
@@ -33,11 +50,27 @@ public class SpriteDirectionController : MonoBehaviour
             // the camera is viewing the slime from
             if (signedAngle < 0f)
             {
-                spriteRenderer.flipX = true;
+                spriteRenderer.flipX = false;
             }
             else
             {
+                spriteRenderer.flipX = true;
+            }
+        }
+        else if (angle < frontLAngle)
+        {
+            // Side animation, in this case the right animation
+            animationDirection = new Vector2(1f, 1f);
+
+            // This changes the side animation based on what side 
+            // the camera is viewing the slime from
+            if (signedAngle < 0f)
+            {
                 spriteRenderer.flipX = false;
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
             }
         }
         else
@@ -45,6 +78,7 @@ public class SpriteDirectionController : MonoBehaviour
             //Front animation
             animationDirection = new Vector2(0f, 1f);
         }
+        
 
         animator.SetFloat("moveX", animationDirection.x);
         animator.SetFloat("moveY", animationDirection.y);
